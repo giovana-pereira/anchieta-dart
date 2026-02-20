ad2ada2e98c6e89b4e9e9494c89c0884c350c7a4
GIOVANA PEREIRA GUSTAVO (2400539) <2400539@edu.anchieta.br>
2026-02-20 20:42:06 -0300

    Exercícios dart

--- lib/exercicios.dart
+++ lib/exercicios.dart
@@ -1,26 +1,47 @@
 // 1. Soma
 num somar(num a, num b){
-  return 0;
+  print('$a + $b = ${a + b}');
+  return a + b;
+
 }
 
+
 // 2. Par
+
 bool isPar(int n) {
-  return false;
+  if (n % 2 == 0) {
+    print('$n é um valor par');
+    return true;
+
+  } else {
+    print('$n é um valor impar');
+    return false;
+  }
 }
 
 // 3. Maior de idade
 bool podeEntrar(int idade){
-  return false;
+    if (idade >= 18) {
+      print('Maior de Idade');
+      return true;
+
+    } else {
+      print('Menor de Idade');
+      return false;
+    }
 }
 
 // 4. Celsius para Fahrenheit
 double converter(double c){
-  return 0.0;
+  print('$c°C equivale a $converter°F');
+  return (c * 1.8) + 32;
 }
+
+
 
 // 5. Saudação
 String saudar(String nome){
-  return "";
+  return ('Olá, $nome!');
 }
 
 // 6. Média
